8df57701732595faa403bc14a3453ec495d3c6c2
truyentdm <truyentdm@gmail.com>
2021-02-03 22:12:06 +0700

    Update data_thebesttipsca.js

--- data_thebesttipsca.js
+++ data_thebesttipsca.js
@@ -1,6 +1,28 @@
 function getData(){
 	var dataAMZ = {
-    		B01KGRAGRS: {href: "https://gg",slug: ""}
+		B01MCUXJX1: {href: "https://amzn.to/2MM0fAS",slug: ""},
+		B00Q7EPSHI: {href: "https://amzn.to/3rgjgKG",slug: ""},
+		B00474X5DO: {href: "https://amzn.to/3cBu2qM",slug: ""},
+		B003OBUE5I: {href: "https://amzn.to/2O26GR0",slug: ""},
+		B00UCIOWRM: {href: "https://amzn.to/3ayORQS",slug: ""},
+		B07S864GPW: {href: "https://amzn.to/3cDcjiE",slug: ""},
+		B002WKJ5ES: {href: "https://amzn.to/3pPeiEo",slug: ""},
+		B074QWYM22: {href: "https://amzn.to/3riwhmV",slug: ""},
+		B008UHXUSO: {href: "https://amzn.to/36z43fV",slug: ""},
+		B003145K3E: {href: "https://amzn.to/39Hr5D6",slug: ""},
+		B071HNQSCM: {href: "https://amzn.to/36C1Zng",slug: ""},
+		B003IBGL7O: {href: "https://amzn.to/39FMw7I",slug: ""},
+		B08NT4YBQ3: {href: "https://amzn.to/3riwOoV",slug: ""},
+		B00JJ5I7GQ: {href: "https://amzn.to/3oKXwVq",slug: ""},
+		B06W54K29J: {href: "https://amzn.to/2MV4sT2",slug: ""},
+		B07J31T4NC: {href: "https://amzn.to/3pOuIgf",slug: ""},
+		B08D3HW1MQ: {href: "https://amzn.to/3rfYhYo",slug: ""},
+		B0128T2HQK: {href: "https://amzn.to/3cDZgOa",slug: ""},
+		B07RSM91SY: {href: "https://amzn.to/2MubUVb",slug: ""},
+		B01DLPUWL2: {href: "https://amzn.to/39HNIHC",slug: ""},
+		B07F3FTJCM: {href: "https://amzn.to/2O4Ucbh",slug: ""},
+		B07GXZ49DY: {href: "https://amzn.to/3tp0fHF",slug: ""},
+		B01DE8PN10: {href: "https://amzn.to/36Dpa0K",slug: ""}
 	}
 	return dataAMZ;
 }
