accab55f59ed59ed3d6d5ae64c9f2b20b1ed8f75
truyentdm <truyentdm@gmail.com>
2021-03-04 23:43:06 +0700

    Create shortlycontent.js

---
+++ shortlycontent.js
@@ -0,0 +1,24 @@
+function shortlyContent(){
+  let data = [
+	{title: `Zinus Upholstered Button Tufted Full Platform Bed with Wooden Slats`, content: `
+		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.\n
+		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button\n
+		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food\n
+		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.\n
+		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors\n
+		- Power supply: 120V – 60Hz\n
+		<img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/>
+		\n
+		<h3>Pros And Cons</h3>
+		<b>+ Pros</b>\n
+		- Safe\n
+		- Easy to use\n
+		- Lots of features\n
+		- Convenient\n
+		<b>+ Cons</b>\n
+		- Can be picky, like the amount of liquid\n
+		- Eventually having to replace seal\n
+	`}
+]
+  return data;
+}
