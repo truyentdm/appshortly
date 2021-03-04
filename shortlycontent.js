842c3270cd4452b28c040cef0750ad8fc2e7a1c4
truyentdm <truyentdm@gmail.com>
2021-03-05 00:30:21 +0700

    Update shortlycontent.js

--- shortlycontent.js
+++ shortlycontent.js
@@ -1,23 +1,23 @@
 function shortlyContent(){
   let data = [
-	{title: `Zinus Upholstered Button Tufted Full Platform Bed with Wooden Slats`, content: `
-		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.\n
-		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button\n
-		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food\n
-		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.\n
-		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors\n
-		- Power supply: 120V – 60Hz\n
+	{title: `Instant Pot Duo 7-in-1 Electric Pressure Cooker`, content: `
+		- Duo, the number 1 selling multi-cooker, combines 7 kitchen appliances in 1. Pressure Cooker, Slow Cooker, Rice Cooker, Steamer, Sauté, Yogurt Marker and Warmer. Prepares dishes up to 70% faster to support your busy lifestyle.<br/>
+		- Features 14 Smart Programs – Soup, Meat/Stew, Bean/Chili, Poultry, Sauté/Simmer, Rice, Multigrain, Porridge, Steam, Slow Cook, Keep Warm, Yogurt, Manual, and Pressure Cook. Now, your favorite dishes are as easy as pressing a button<br/>
+		- Healthy, stainless steel (18/8) inner cooking pot made from food grade 304, no chemical coating, 3-ply bottom for even heat distribution. Fully sealed environment traps the flavours, nutrients and aromas within the food<br/>
+		- Built with the latest 3rd generation technology, the microprocessor monitors pressure, temperature, keeps time, and adjusts heating intensity and duration to achieve your desired results every time.<br/>
+		- UL and ULC certified. With 10 safety mechanisms to provide you with added assurance, designed to eliminate many common errors<br/>
+		- Power supply: 120V – 60Hz<br/>
 		<img src="https://1.bp.blogspot.com/-MWw7WGwLzcI/YBZkvBBA8OI/AAAAAAAABoI/wxopuyheqzE1TfA1X5v37HVwIqhSQbK9wCLcBGAsYHQ/s574/4.jpg"/>
-		\n
+		<br/>
 		<h3>Pros And Cons</h3>
-		<b>+ Pros</b>\n
-		- Safe\n
-		- Easy to use\n
-		- Lots of features\n
-		- Convenient\n
-		<b>+ Cons</b>\n
-		- Can be picky, like the amount of liquid\n
-		- Eventually having to replace seal\n
+		<b>+ Pros</b><br/>
+		- Safe<br/>
+		- Easy to use<br/>
+		- Lots of features<br/>
+		- Convenient<br/>
+		<b>+ Cons</b><br/>
+		- Can be picky, like the amount of liquid<br/>
+		- Eventually having to replace seal<br/>
 	`}
 ]
   return data;
