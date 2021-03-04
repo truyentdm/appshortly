c68c6fb4c9b5f860faa622d83e87528b00ac6af3
truyentdm <truyentdm@gmail.com>
2021-03-05 01:46:27 +0700

    Update shortlycontent.js

--- shortlycontent.js
+++ shortlycontent.js
@@ -18,6 +18,24 @@ function shortlyContent(){
 		<b>+ Cons</b><br/>
 		- Can be picky, like the amount of liquid<br/>
 		- Eventually having to replace seal<br/>
+	`},{title: `Dash Deluxe Electric Air Fryer + Oven Cooker`, content: `
+		- HEALTHIER FRIED FOOD: Dash deluxe air fryer uses air crisp technology (instead of oil) to help reduce added fat by 70-80%, without sacrificing the flavor of your fried food (no more residual smell hours after you’ve eaten)<br/>
+		- 6 QUART CAPACITY: The PERFECT appliance for picky eaters, large families, entertaining, or those who have busy schedules. 6qt basket is larger, and makes an abundance of appetizers, desserts, chicken wings, french fries, even baked goods, and all within minutes of setting the timer – it couldn’t be easier!<br/>
+		- QUICK + EASY: Short on time? Simply load the large 6 qt capacity fryer basket, set the temp, and set the timer. Your food comes out crispy, guaranteed, EVERY TIME. And in less time than it takes to pre-heat your oven, making it more energy efficient.<br/>
+		- SAFER: The auto-shut off function prevents overcooking, and the cool touch housing and handle makes it safer to use. It’s so simple, even your kids can use it!<br/>
+		- The nonstick fryer basket is dishwasher safe for even easier cleanup!<br/>
+		- INCLUDES: The 1700-watt Deluxe air fryer is backed by a 1-year manufacturer warranty and includes a recipe book and recipe database access (all non-electric parts are dishwasher safe).<br/>
+		- Available in Black, White, Red, and Aqua.<br/>
+		<div class="short-img"><img src="https://1.bp.blogspot.com/-_BAJmj8ZPIQ/YBlmIx-TGdI/AAAAAAAABos/Yt8aCMZDST0MLjA1oben4GdL3J-dP3DlACLcBGAsYHQ/s600/3.jpg"/></div>
+		<br/>
+		<h3>Pros And Cons</h3>
+		<b>+ Pros</b><br/>
+		- Easy to Use<br/>
+		- Healthier<br/>
+		- Easy to Clean<br/>
+		- Less Power than an Oven<br/>
+		<b>+ Cons</b><br/>
+		- Slow to Cook<br/>
 	`}
 ]
   return data;
