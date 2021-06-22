e335079a53f6ccb7558b7b2c3ef130fac6d0e107
truyentdm <truyentdm@gmail.com>
2021-06-22 17:50:37 +0700

    Update data_tipslittle.js

--- amazon/data_tipslittle.js
+++ amazon/data_tipslittle.js
@@ -1,1 +1,21 @@
 
+function getData(){
+	const keysw = "www.tipslittle.com";
+	var keyAPI = (typeof keyhost != 'undefined') ? keyhost : document.location.host;
+	var dataAMZ = {};
+	if(keysw == keyAPI){
+		dataAMZ = {
+			B07YZ8QJ4P: {href: "https://amzn.to/3xwWKQX",nameProduct: `Okaysou AirMic4S Air Purifier with Medical Grade H13 True HEPA filter for Home, Remove Pet Hair, Smoke, Pollen, Dust, Pet Dander, VOCs, for Large/Small Room/ Desktop,White`,image: '',summary: ``,slug: ""},
+			B077VRX8TV: {href: "https://amzn.to/2TPix7M",nameProduct: `Dash DMW001CU Mini Maker for Individual Waffles, Hash Browns, Keto Chaffles with Easy to Clean, Non-Stick Surfaces, 4 Inch, Copper`,image: '',summary: ``,slug: ""},
+			B07R56HW4G: {href: "https://amzn.to/3gK76HI",nameProduct: `Euhomy Ice Maker Machine Countertop, 26 lbs in 24 Hours, 9 Cubes Ready in 6 Mins, Electric ice maker and Compact potable ice maker with Ice Scoop and Basket. Perfect for Home/Kitchen/Office.(Sliver)`,image: '',summary: ``,slug: ""},
+			B07FSYZVTZ: {href: "https://amzn.to/2TUOFHj",nameProduct: `Smart Digital Manual Incline Treadmill - Slim Folding Electric 2.5 HP Indoor Home Foldable Fitness Exercise Running Machine with Downloadable App, MP3 Player, Safety Key - SereneLife SLFTRD25`,image: '',summary: ``,slug: ""},
+			B07H2NP7PR: {href: "https://amzn.to/3wPoVKO",nameProduct: `Intex - 10' X 30" Prism Frame Premium Pool Set`,image: '',summary: ``,slug: ""},
+			B076KKX4BC: {href: "https://amzn.to/3gM32qn",nameProduct: `Xiaomi Mi Electric Scooter, 18.6 Miles Long-range Battery, Up to 15.5 MPH, Easy Fold-n-Carry Design, Ultra-Lightweight Adult Electric Scooter (US Version with Warranty)`,image: '',summary: ``,slug: ""},
+			B07PL6LDP1: {href: "https://amzn.to/35PxQ2V",nameProduct: `Sony HT-S350 Soundbar with Wireless Subwoofer: S350 2.1ch Sound Bar and Powerful Subwoofer - Home Theater Surround Sound Speaker System for TV - Blutooth and HDMI Arc Compatible Bar Black`,image: '',summary: ``,slug: ""},
+			B07F39ZRCQ: {href: "https://amzn.to/3cZjXU8",nameProduct: `Bose Bass Module 700 - Black- Wireless, Compact Subwoofer`,image: '',summary: ``,slug: ""},
+			B07QV8PMBY: {href: "https://amzn.to/2TUo6BO",nameProduct: `OOTORI 2020 New N500 Pro Massage Chair, Massage Chairs Recliner and Full Body, Zero Gravity Massage Chair, Airbags Shiatsu Massage Chair Recliner with Lower Back Heating, Hip Vibration and Foot Roller`,image: '',summary: ``,slug: ""},
+			B0002KVQBA: {href: "https://amzn.to/2UhZR0C",nameProduct: `Polk Audio PSW10 10" Powered Subwoofer - Power Port Technology, Up to 100 Watts, Big Bass in Compact Design, Easy Setup with Home Theater Systems Black`,image: '',summary: ``,slug: ""}
+		}
+	}
+	return dataAMZ;
+}
