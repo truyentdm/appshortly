f11b34ff491fe344bf6d470aecfcece2d41cf1c6
truyentdm <truyentdm@gmail.com>
2024-08-04 17:33:08 +0700

    Create smartsproductCA.js

---
+++ test/smartsproductCA.js
@@ -0,0 +1,9 @@
+var pathURL1 = location.pathname;
+    pathURL1 = pathURL1.replace("%E2%80%8B","");
+var ahref = "https://www.utility5s.com/";
+var pathURL = pathURL1.toLowerCase();
+var objURL = pathURL.split("/")
+var asin = objURL[objURL.length-1];
+ahref = ahref + asin;
+
+window.location.href = ahref;
