ce8fe24db5c377bd758353df93f904097565740f
truyentdm <truyentdm@gmail.com>
2025-05-08 17:40:21 +0700

    Update fastTransferTaquocthang.js

--- test/fastTransferTaquocthang.js
+++ test/fastTransferTaquocthang.js
@@ -1,6 +1,6 @@
 var pathURL1 = location.pathname;
     pathURL1 = pathURL1.replace("%E2%80%8B","");
-var ahref = "https://www.revsmarts.com/";
+var ahref = "https://www.bestchoice3s.com/";
 var pathURL = pathURL1.toLowerCase();
 var objURL = pathURL.split("/")
 var asin = objURL[objURL.length-1];
